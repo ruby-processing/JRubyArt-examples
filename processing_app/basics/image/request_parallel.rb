3ad4f234e5bb6fe1403e28ad2d0e553ecd8436fd
monkstone <mamba2928@yahoo.co.uk>
2016-05-22 07:21:11 +0100

    using parallel gem with JRubyArt

---
+++ processing_app/basics/image/request_parallel.rb
@@ -0,0 +1,44 @@
+# Request Image
+# by Ira Greenberg.
+# From Processing for Flash Developers, Friends of ED, 2009.
+#
+# Shows how to use the request_image() function.
+# The request_image() function loads images on a separate thread so that
+# the sketch does not freeze while they load.  
+# These images are small for a quick download, but try it with your own huge 
+# images to get the full effect.
+#
+# See how to pad a number string in ruby using 'rjust'
+require 'parallel'
+
+attr_reader :imgs
+
+def setup
+  sketch_title 'Request Image'
+  # Load images asynchronously, kind of pointless with this few small images
+  @imgs = Parallel.map(0...10) do |i|
+    request_image(
+      data_path(format('PT_anim%s.gif', i.to_s.rjust(4, '0'))
+      )
+    )
+  end 
+end
+
+def draw
+  background 0
+  # put pre-load animation here?
+  # When all images are loaded draw them to the screen
+  return unless all_loaded?
+  imgs.each_with_index do |img, i|
+    image(img, width / imgs.length * i, 0, width / imgs.length, height)
+  end
+end
+
+# Return true when all images are loaded 
+def all_loaded?
+  imgs.length == 10
+end
+
+def settings
+  size 1290, 120
+end
