67b6590c3c198c57c0c7573fdfc5a4df71edabd7
monkstone <mamba2928@yahoo.co.uk>
2016-05-25 14:54:11 +0100

    proximity example

---
+++ external_library/java/hype/proximity.rb
@@ -0,0 +1,62 @@
+# encoding: utf-8
+load_library :hype
+include_package 'hype'
+java_import 'hype.extended.behavior.HProximity'
+java_import 'hype.extended.behavior.HOscillator'
+
+PALETTE = %w(#242424 #00FF00 #FF3300 #4D4D4D).freeze
+KEY = %i(background fill_one fill_two stroke).freeze
+
+attr_reader :colors
+
+def settings
+  size(640, 640)
+end
+
+def setup
+  sketch_title 'Hype Proximity'
+  @colors = KEY.zip(web_to_color_array(PALETTE)).to_h
+  H.init(self)
+  H.background(colors[:background])
+  canvas = H.add(HCanvas.new.auto_clear(false).fade(5))
+  r1 = HRect.new(5)
+  r1.no_stroke
+    .fill(colors[:fill_one])
+    .loc(width/2, height/2)
+    .rotate(45)
+    .anchor_at(H::CENTER)
+  canvas.add(r1)
+  r2 = HRect.new(5, 10)
+            .rounding(4)
+  r2.no_stroke
+    .fill(colors[:fill_two])
+    .x(40)
+    .y(height / 2)
+    .anchor_at(H::CENTER)
+  canvas.add(r2)
+  HProximity.new
+            .target(r2)
+            .neighbor(r1)
+            .property(H::HEIGHT)
+            .spring(0.99)
+            .ease(0.7)
+            .min(10)
+            .max(200)
+            .radius(250)
+  HOscillator.new
+             .target(r2)
+             .property(H::X)
+             .range(40, 600)
+             .speed(5)
+             .freq(0.5)
+             .current_step(-180)
+end
+
+def draw
+  H.draw_stage
+  # outline to show area of proximity  
+  ellipse_mode(CENTER)
+  stroke(colors[:stroke])
+  no_fill
+  ellipse(width / 2, height / 2, 500, 500)
+end
