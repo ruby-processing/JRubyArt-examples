d1adaa42144feeb17e45cf15e74edf9aad72f0bb
monkstone <mamba2928@yahoo.co.uk>
2016-06-05 14:56:49 +0100

    damn tabs

---
+++ external_library/java/hype/colorist.rb
@@ -0,0 +1,38 @@
+load_library 'hype'
+
+java_import 'hype.extended.colorist.HPixelColorist'
+java_import 'hype.extended.layout.HGridLayout'
+include_package 'hype'
+
+CELL_SIZE = 25
+
+def settings
+  size(618, 482)
+end
+
+def setup
+  sketch_title 'Colorist'
+  H.init(self)
+  H.background(color('#242424'))
+
+  colors = HPixelColorist.new(data_path('phil_pai.jpg'))
+                         .fill_only
+  # .strokeOnly
+  # .fillAndStroke
+  pool = HDrawablePool.new(576)
+  pool.autoAddToStage
+      .add(HRect.new.rounding(4))
+      .layout(
+        HGridLayout.new
+                   .start_x(21)
+                   .start_y(21)
+                   .spacing(CELL_SIZE + 1, CELL_SIZE + 1)
+                   .cols(24)
+      )
+      .on_create do |obj|
+    obj.no_stroke.anchor_at(H::CENTER).size(CELL_SIZE)
+    colors.applyColor(obj)
+  end.request_all
+  H.draw_stage
+  no_loop
+end
