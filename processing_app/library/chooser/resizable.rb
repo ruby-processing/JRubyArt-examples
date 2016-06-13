51486048b49625a1657c3af1d4e59c4d25994f5a
monkstone <mamba2928@yahoo.co.uk>
2016-06-13 14:52:26 +0100

    file chooser

---
+++ processing_app/library/chooser/resizable.rb
@@ -0,0 +1,33 @@
+load_library :chooser
+
+attr_reader :img
+
+def settings
+  size(400, 200)
+end
+
+def setup
+  sketch_title 'Chooser'
+  resizable
+  fill 0, 0, 200
+  text('Click Window to Load Image', 10, 100)
+end
+
+def draw
+  image(img, 0, 0) unless img.nil?
+end
+
+def file_selected(selection)
+  if selection.nil?
+    puts 'Nothing Chosen'
+  else
+    @img = load_image(selection.get_absolute_path)
+    surface.set_size(img.width, img.height)
+  end
+end
+
+def mouse_clicked
+  @img = nil
+  # java_signature 'void selectInput(String, String)'
+  select_input('select an image', 'file_selected')
+end
