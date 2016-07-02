b0b967d5f21ac1ecf6d62286050f4906eb8ca78b
monkstone <mamba2928@yahoo.co.uk>
2016-07-02 14:51:44 +0100

    so much neater in ruby

--- processing_app/library/video/capture/mirror.rb
+++ processing_app/library/video/capture/mirror.rb
@@ -17,10 +17,8 @@ attr_reader :cols, :rows, :video
 def setup
   sketch_title 'Mirror'
   frameRate(30)
-  @cols = width / CELL_SIZE
-  @rows = height / CELL_SIZE
   color_mode(RGB, 255, 255, 255, 100)
-  @video = Java::ProcessingVideo::Capture.new(self, width, height)
+  @video = Capture.new(self, width, height)
   # Start capturing the images from the camera
   video.start
   background(0)
@@ -29,14 +27,11 @@ end
 def draw
   video.load_pixels
   # Begin loop for columns
-  cols.times do |i|
+  (0...width).step(CELL_SIZE) do |i|
     # Begin loop for rows
-    rows.times do |j|
-      # Where are we, pixel-wise?
-      x = i * CELL_SIZE
-      y = j * CELL_SIZE
+    (0...height).step(CELL_SIZE) do |j|
       # Reversing x to mirror the image
-      loc = (video.width - x - 1) + y * video.width
+      loc = (width - i - 1) + j * width
       r = red(video.pixels[loc])
       g = green(video.pixels[loc])
       b = blue(video.pixels[loc])
@@ -45,7 +40,7 @@ def draw
       # Code for drawing a single rect
       # Using translate in order for rotation to work properly
       push_matrix
-      translate(x + CELL_SIZE / 2, y + CELL_SIZE / 2)
+      translate(i + CELL_SIZE / 2, j + CELL_SIZE / 2)
       # Rotation formula based on brightness
       rotate((2 * PI * brightness(c) / 255.0))
       rect_mode(CENTER)
