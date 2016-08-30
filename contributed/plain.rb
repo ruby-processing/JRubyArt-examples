94bb564e24d66e9c70a5aa1125de05e5678e7394
monkstone <mamba2928@yahoo.co.uk>
2016-08-30 19:38:47 +0100

    fix Rakefiles

--- contributed/plain.rb
+++ contributed/plain.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 # frozen_string_literal: true
 # Description:
-# This is a full-screen demo 
+# This is a full-screen demo
 # Since processing-3.0a10 set in settings
 
 class FullScreen < Processing::App
@@ -13,19 +13,18 @@ class FullScreen < Processing::App
   end
 
   def draw
-    background 200
+    background 0
     fill 120, 160, 220
     stroke 0
     rect_mode(CENTER)
     (dim..width - dim).step(dim) do |x|
       (dim..height - dim).step(dim) do |y|
-        rect x, y, dim, dim, rnd, rnd, rnd, rnd 
+        rect x, y, dim, dim, rnd, rnd, rnd, rnd
       end
     end
-  end  
+  end
 
   def settings
     full_screen
   end
 end
-
--- processing_app/topics/vectors/vector_math.rb
+++ processing_app/topics/vectors/vector_math.rb
@@ -1,34 +1,33 @@
 #
-# Vector 
-# by Daniel Shiffman.  
-# 
+# Vector
+# by Daniel Shiffman.
+#
 # Demonstration some basic vector math: subtraction, normalization, scaling
 # Normalizing a vector sets its length to 1. See library/vecmath for example
 # using the more ruby like Vec2D class instead of Vec2D
 #
-
 
 def setup
   sketch_title 'Vector Math'
 end
 
 def draw
-  background(0)  
+  background(0)
   # A vector that points to the mouse location
   mouse = Vec2D.new(mouse_x, mouse_y)
   # A vector that points to the center of the window
   center = Vec2D.new(width / 2,height / 2)
   # Subtract center from mouse which results in a vector that points from center to mouse
-  mouse -= center  
+  mouse -= center
   # Normalize the vector
-  mouse.normalize!  
+  mouse.normalize!
   # Multiply its length by 150 (Scaling its length)
   mouse *= 150
   translate(width / 2,height / 2)
   # Draw the resulting vector
   stroke(255)
   stroke_weight(4)
-  line(0, 0, mouse.x, mouse.y)  
+  line(0, 0, mouse.x, mouse.y)
 end
 
 def settings
