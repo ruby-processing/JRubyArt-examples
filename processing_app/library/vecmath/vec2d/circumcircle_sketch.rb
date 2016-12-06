a2ba74a82032813d53c0bfc8720df4f7950c16a0
monkstone <mamba2928@yahoo.co.uk>
2016-12-06 16:06:41 +0000

    circumcircle

---
+++ processing_app/library/vecmath/vec2d/circumcircle_sketch.rb
@@ -0,0 +1,89 @@
+# Loosely based on a sketch by Bárbara Almeida
+# https://www.openprocessing.org/sketch/179567
+# Here is a sketch that clearly demonstrates some of the most egregious parts of
+# vanilla processing:-
+# PVector is over complicated and overloads 2D and 3D functionality, cf Vec2D
+# JRubyArt and propane which behaves as 2D vector (cross product is a float) and
+# can be used in chain calculations.
+# The inverted Y axis in processing requires remapping to use in graphs etc.
+# The map method of processing can be used to this remapping, however in many
+# languages (python, ruby etc) map is a keyword with alternative usage.
+# For this reason in JRubyArt and propane we have replaced vanilla processing
+# map with map1d.
+
+##################################################
+# Usage click on screen with mouse to generate a point, repeat as required
+# to create a triangle, and the circumcircle will be drawn unless the points are
+# collinear. Additional clicks erase the first point and add a new point. To
+# demonstrate collinear test press 'c' or 'C', press space-bar to clear
+##################################################
+
+load_library :circle
+
+attr_reader :point, :font, :points, :circle, :center
+
+def settings
+  size 800, 800
+  # pixel_density(2) # for HiDpi screens
+  # smooth # see https://processing.org/reference/smooth_.html
+end
+
+def setup
+  sketch_title 'Circumcircle Through Three Points'
+  @point = MathPoint.new
+  @font = create_font('Arial', 16, true)
+  @points = TPoints.new
+  ellipse_mode RADIUS
+end
+
+def draw
+  graph_paper
+  point.display
+  points.map(&:display)
+  return unless circle
+  center.display
+  no_fill
+  stroke 200
+  triangle(
+    points[0].screen_x,
+    points[0].screen_y,
+    points[1].screen_x,
+    points[1].screen_y,
+    points[2].screen_x,
+    points[2].screen_y
+  )
+  ellipse(center.screen_x, center.screen_y, circle.radius, circle.radius)
+end
+
+def mouse_pressed
+  points << MathPoint.new.from_sketch(mouse_x, mouse_y)
+  return unless points.full?
+  @circle = Circumcircle.new(points.array)
+  circle.calculate
+  @center = MathPoint.new(circle.center)
+end
+
+def key_pressed
+  case key
+  when ' '
+    points.clear
+  when 'c', 'C'
+    points << MathPoint.new(Vec2D.new(0, 0))
+    points << MathPoint.new(Vec2D.new(100, 100))
+    points << MathPoint.new(Vec2D.new(300, 300))
+    puts 'collinear points' if points.collinear?
+  end
+end
+
+def graph_paper
+  background(0)
+  cell_size = 20 # size of the grid
+  dim = cell_size * 2
+  (dim..width - dim).step(cell_size) do |i|
+    stroke(75, 135, 155)
+    line(i, dim, i, height - dim)
+  end
+  (dim..height - dim).step(cell_size) do |i|
+    line(dim, i, width - dim, i)
+  end
+end
---
+++ processing_app/library/vecmath/vec2d/library/circle/circle.rb
@@ -0,0 +1,4 @@
+require_relative 'lib/circumcircle'
+require_relative 'lib/math_point'
+require_relative 'lib/points'
+
