4dcc1533624219abbc7b800ea06575595930799b
monkstone <mamba2928@yahoo.co.uk>
2017-03-12 15:56:19 +0000

    join words

--- external_library/gem/ruby_wordcram/fruits.rb
+++ external_library/gem/ruby_wordcram/fruits.rb
@@ -1,27 +1,27 @@
 # Literate colors from JRubyArt
 # uses `web_to_color_array` to create a hash of fruity colors
+# to join two words as `cherry\ red` use back slash 
 require 'ruby_wordcram'
 
-PALETTE = %w(#b2c248 #a61733 #f7ca18 #32cd23 #ffc800 #f27935 #ffe5b4 #913d88 #f22613 #ff43a4).freeze
-FRUITS = %w(avocado cranberry lemon lime mango orange peach plum pomegranate strawberry).freeze
+PALETTE = %w(#b2c248 #c40000 #a61733 #f7ca18 #32cd23 #ffc800 #f27935 #ffe5b4 #913d88 #f22613 #c72c48 #ff43a4).freeze
+FRUITS = %w(avocado cherry\ red cranberry lemon lime mango orange peach plum pomegranate raspberry strawberry).freeze
 
 def settings
-  size 800, 500
+  size 500, 500
 end
 
 def setup
   sketch_title 'Fruity Colors'
   background(255)
   colors = FRUITS.zip(web_to_color_array(PALETTE)).to_h
-  count = 0
   words = FRUITS.map do |fruit|
-    word = Word.new(fruit, count)
-    count += 1
+    weight = norm_strict(fruit.length, 11, 4)
+    word = Word.new(fruit, weight)
     word.set_font(create_font(data_path('MINYN___.TTF'), 1))
     word.set_color(colors[fruit])
   end
-  @wc = WordCram.new(self)
+  WordCram.new(self)
                 .from_words(words.to_java(Word))
-                .sized_by_weight(90, 30)
+                .sized_by_weight(40, 80)
                 .draw_all
 end
