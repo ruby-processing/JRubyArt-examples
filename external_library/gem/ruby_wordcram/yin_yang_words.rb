e763c0ff5a1b5f8dbc6c0292a9791b7dd436acab
monkstone <mamba2928@yahoo.co.uk>
2017-03-14 15:28:27 +0000

    this is ruby it should be elegant 'dry'

--- external_library/gem/ruby_wordcram/yin_yang_words.rb
+++ external_library/gem/ruby_wordcram/yin_yang_words.rb
@@ -9,31 +9,36 @@ def setup
   background 255
   image = load_image(data_path('yinyang.png'))
   image.resize(width, height)
-  image_shape = ImageShaper.new.shape(image, color('#000000'))
+  create_image_shaped_wordcram(
+    image: image,
+    mask: color('#000000'),
+    words: repeat_word('flexible', 500),
+    hue: color('#F5B502')
+  )
+  create_image_shaped_wordcram(
+    image: image,
+    mask: color('#ffffff'),
+    words: repeat_word('usable', 500),
+    hue: color('#782CAF')
+  )
+end
+
+def create_image_shaped_wordcram(image:, mask:, words:, hue:)
+  image_shape = ImageShaper.new.shape(image, mask)
   placer = ShapeBasedPlacer.new(image_shape)
   WordCram.new(self)
-          .from_words(repeat_word('flexible', 500).to_java(Word))
+          .from_words(words.to_java(Word))
           .with_placer(placer)
           .with_nudger(placer)
           .sized_by_weight(4, 40)
           .angled_at(0)
-          .with_color(color('#F5B502'))
+          .with_color(hue)
           .draw_all
-  image_shape = ImageShaper.new.shape(image, color('#ffffff'))
-  placer = ShapeBasedPlacer.new(image_shape)
-  WordCram.new(self)
-          .from_words(repeat_word('usable', 500).to_java(Word))
-          .with_placer(placer)
-          .with_nudger(placer)
-          .sized_by_weight(4, 40)
-          .angled_at(0)
-          .with_color(color('#782CAF'))
-          .draw_all
- end
+end
 
 def repeat_word(word, times)
   (0..times).map do
     # Give the words a random weight, so they're sized differently.
     Word.new(word, rand)
-   end
- end
+  end
+end
