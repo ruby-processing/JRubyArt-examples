1de5b8bb8bcbd30402b01b0e2efe3f0933782ec6
monkstone <mamba2928@yahoo.co.uk>
2017-09-27 20:18:08 +0100

    poisson sampling

---
+++ external_library/java/PixelFlow/sampling_poisson2d.rb
@@ -0,0 +1,78 @@
+
+#
+# PixelFlow | Copyright (C) 2016 Thomas Diewald - http://thomasdiewald.com
+#
+# A Processing/Java library for high performance GPU-Computing (GLSL).
+# MIT License: https://opensource.org/licenses/MIT
+#
+
+
+
+ load_library :PixelFlow
+ java_import 'com.thomasdiewald.pixelflow.java.sampling.PoissonSample'
+ java_import 'com.thomasdiewald.pixelflow.java.sampling.PoissonDiscSamping2D'
+ attr_reader :samples, :display_radius
+
+  def settings
+    size(1280, 720, P2D)
+    smooth(16)
+  end
+
+  def setup
+    sketch_title 'Poisson Sampling 2D'
+    @display_radius = true
+    generate_poisson_sampling2d
+  end
+
+
+  def generate_poisson_sampling2d
+    pds = RealDiscSamping2D.new
+    bounds = [0, 0, 0, width, height, 0]
+    rmin = 2
+    rmax = 25
+    roff = 0.5
+    new_points = 100
+    start = Time.now
+    pds.setRandomSeed(rand(0..10_0000))
+    pds.generatePoissonSampling2D(bounds, rmin, rmax, roff, new_points)
+    time = Time.now - start
+    puts("poisson samples 2D generated")
+    puts("    time: #{(time * 1000).floor}ms")
+    puts("    count: #{pds.samples.size}")
+    @samples = pds.samples
+  end
+
+  def draw
+    background(64)
+    samples.each do |sample|
+      px = sample.x
+      py = sample.y
+      pr = sample.rad
+      if display_radius
+        stroke(255)
+        stroke_weight(0.5)
+        fill(255)
+        no_stroke
+        ellipse(px, py, pr * 2, pr * 2)
+      else
+        stroke(255)
+        stroke_weight(2)
+        point(px, py)
+      end
+    end
+  end
+
+  def key_released
+    case key
+    when ' '
+      @display_radius = !display_radius
+    when 'r'
+      generate_poisson_sampling2d
+    end
+  end
+
+  class RealDiscSamping2D < PoissonDiscSamping2D
+    def newInstance(x, y, r, rcollision)
+      PoissonSample.new(x, y, r, rcollision)
+    end
+  end
