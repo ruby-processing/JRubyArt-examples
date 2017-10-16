21e4dce0cea66af36fd6464c1d7feb015983e3b8
monkstone <mamba2928@yahoo.co.uk>
2017-10-16 13:48:30 +0100

    button group

---
+++ external_library/java/controlP5/button_group.rb
@@ -0,0 +1,33 @@
+load_library :controlP5
+include_package 'controlP5'
+
+attr_reader :r1, :back_color
+
+def settings
+  size(400,400)
+end
+
+def setup
+  sketch_title 'Button Group'
+  @back_color = color(0, 0, 0)
+  cp5 = ControlP5.new(self)
+  group_fluid = cp5.addGroup('groceries')
+                   .setPosition(20,20)
+                   .setBackgroundHeight(100)
+                   .setBackgroundColor(color(255,50))
+  cp5.addButton('fish')
+     .setPosition(10,20)
+     .setSize(80,20)
+     .setGroup('groceries')
+     .addListener { |event| puts event }
+  cp5.addButton('fruit')
+     .setPosition(10,60)
+     .setSize(80,20)
+     .setGroup('groceries')
+     .addListener { |event| puts event }
+end
+
+
+def draw
+  background(back_color)
+end
