5a3133e05b4dac2a898ae74c7920502f65ca9047
monkstone <mamba2928@yahoo.co.uk>
2017-11-23 10:54:02 +0000

    Make it work again

--- processing_app/topics/advanced_data/load_save_yaml.rb
+++ processing_app/topics/advanced_data/load_save_yaml.rb
@@ -1,89 +1,109 @@
-######################################
-# Yet another examples of reading and
-# writing to some form of markup,
-# appropriately yaml.
-# by Martin Prout after Dan Shiffman
-# ###################################
-require 'forwardable'
-require 'psych'
-load_library :bubble
+require 'yaml'
 
-attr_reader :bubble_data
+attr_reader :bubbles, :bubble_data
+
+ MAX_BUBBLE = 10
+
+def settings
+  size(640, 360)
+end
 
 def setup
-  sketch_title 'Load Save Yaml'
-  # load data from file
-  @bubble_data = BubbleData.new 'bubbles'
-  bubble_data.load_data(data_path('data.yml'))
+  sketch_title 'Bubble Yaml'
+  # read source_string from file
+  load_data
 end
 
 def draw
   background 255
-  bubble_data.display mouse_x, mouse_y
+  bubbles.each do |bubble|
+    bubble.display
+    bubble.rollover(mouse_x, mouse_y)
+  end
+end
+
+def load_data
+  yaml = YAML.load_file(data_path('data.yml'))
+  # parse the source string
+  @bubble_data = BubbleData.new 'bubbles'
+  # get the bubble_data from the top level hash
+  data = bubble_data.extract_data yaml
+  # map the bubble_data array to an array of bubbles
+  @bubbles = data.map do |point|
+    pos = point['position']
+    Bubble.new(pos['x'], pos['y'], point['diameter'], point['label'])
+  end
+end
+
+def save_data
+  # demonstrate how easy it is to create json object from a hash in ruby
+  hash = bubble_data.to_hash
+  # overwite existing 'data.yml'
+  open(data_path('data.yml'), 'w') { |file| file.write(hash.to_yaml) }
 end
 
 def mouse_pressed
   # create a new bubble instance, where mouse was clicked
-  @bubble_data.create_new_bubble(mouse_x, mouse_y)
+  @bubble_data.add_bubble(
+    Bubble.new(mouse_x, mouse_y, rand(40..80), 'new label')
+  )
+  save_data
+  # reload the yaml data from the freshly created file
+  load_data
 end
 
-# Bubble class can create and display bubble data from a yaml file
+# Bubble data holder
 class BubbleData
-  extend Forwardable
-  def_delegators(:@bubbles, :each, :size)
-  include Enumerable
-
-  MAX_BUBBLE = 10
-
-  attr_reader :key
-  def initialize(key)
+  attr_reader :name, :data
+  def initialize(name = 'bubbles')
+    @name = name
     @bubbles = []
-    @key = key
-    super
   end
 
-  def create_new_bubble(x, y)
-    add(Bubble.new(x, y, rand(40..80), 'new label'))
-    save_data
-    load_data path
+  def add_bubble(bubble)
+    bubbles << bubble
+    bubbles.shift if bubbles.size > MAX_BUBBLE
   end
 
-  def display(x, y)
-    each do |bubble|
-      bubble.display
-      bubble.rollover(x, y)
-    end
+  def extract_data(yaml)
+    @bubbles = yaml[name]
   end
 
-  # @param path to yaml file
-  def load_data(path)
-    yaml = Psych.load_file(path)
-    data = yaml[key]
-    clear
-    # iterate the bubble_data array, and create an array of bubbles
-    data.each do |point|
-      add Bubble.new(
-        point['position']['x'],
-        point['position']['y'],
-        point['diameter'],
-        point['label'])
-      end
-    end
+  def to_hash
+    { name => bubbles.map(&:to_hash) }
+  end
+end
 
-    def add(bubble)
-      bubbles << bubble
-      bubbles.shift if bubbles.size > MAX_BUBBLE
-    end
+# The Bubble class
+class Bubble
+  attr_reader :x, :y, :diameter, :name, :over
 
-    private
-
-    def save_data
-      hash = { key => map(&:to_hash) }
-      # overwite existing 'data.yaml'
-      File.write(data_path('data.yml'), hash.to_yaml)
-    end
+  def initialize(x, y, diameter, name)
+    @x, @y, @diameter, @name = x, y, diameter, name
+    @over = false
   end
 
-  def settings
-    size 640, 360
+  def rollover(px, py)
+    distance = dist px, py, x, y
+    @over = (distance < diameter / 2.0)
   end
+
+  def display
+    stroke 0
+    stroke_weight 2
+    no_fill
+    ellipse x, y, diameter, diameter
+    return unless over
+    fill 0
+    text_align CENTER
+    text(name, x, y + diameter / 2.0 + 20)
+  end
+
+  def to_hash
+    {
+      'position' => { 'x' => x, 'y' => y },
+      'diameter' => diameter,
+      'label' => name
+    }
+  end
+end
