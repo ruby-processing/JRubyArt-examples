aa5803413f775482bc2488c1e971631976fffd03
monkstone <mamba2928@yahoo.co.uk>
2018-01-30 10:47:29 +0000

    oscillator example

---
+++ external_library/java/hype/h_oscillator.rb
@@ -0,0 +1,102 @@
+load_library :hype
+
+HYPE = %w[H HCanvas HDrawablePool HRect].freeze
+EXTENDED = %w[
+  behavior.HTimer behavior.HOscillator colorist.HPixelColorist
+].freeze
+hype_format = 'hype.%s'
+hype_extended_format = 'hype.extended.%s'
+HYPE.each { |klass| java_import format(hype_format, klass) }
+EXTENDED.each { |klass| java_import format(hype_extended_format, klass) }
+
+attr_reader :canvas, :colors, :pool, :xo, :ao, :wo, :ro, :zo
+
+def setup
+  sketch_title 'Oscillator'
+  H.init(self)
+  H.background(color(0xff000000))
+  H.use3D(true)
+  @colors = HPixelColorist.new(data_path('gradient.jpg'))
+  @canvas = HCanvas.new(P3D).autoClear(true)
+  H.add(canvas)
+  @pool = HDrawablePool.new(1_000)
+  pool.autoParent(canvas)
+  .add(HRect.new.rounding(10))
+  .on_create do |obj|
+    i = pool.current_index
+    obj.no_stroke
+    .size(rand(40..80), rand(60..80))
+    .loc(rand(width), rand(height))
+    .anchorAt(H::CENTER)
+    @xo = HOscillator.new
+    .target(obj)
+    .property(H::X)
+    .relativeVal(obj.x)
+    .range(rand(-10..-5), rand(5..10))
+    .speed(rand(0.005..0.2))
+    .freq(10)
+    .current_step(i)
+    @ao = HOscillator.new
+    .target(obj)
+    .property(H::ALPHA)
+    .range(50, 255)
+    .speed(rand(0.3..0.9))
+    .freq(5)
+    .current_step(i)
+    @wo = HOscillator.new
+    .target(obj)
+    .property(H::WIDTH)
+    .range(-obj.width, obj.width)
+    .speed(rand(0.05..0.2))
+    .freq(10)
+    .current_step(i)
+    @ro = HOscillator.new
+    .target(obj)
+    .property(H::ROTATION)
+    .range(-180, 180)
+    .speed(rand(0.005..0.05))
+    .freq(10)
+    .current_step(i)
+    @zo = HOscillator.new
+    .target(obj)
+    .property(H::Z)
+    .range(-400, 400)
+    .speed(rand(0.005..0.01))
+    .freq(15)
+    .current_step(i * 5)
+  end
+  .onRequest do |obj|
+    obj.scale(1).alpha(0).loc(rand(width), rand(height), -rand(200))
+    xo.register
+    ao.register
+    wo.register
+    ro.register
+    zo.register
+  end.onRelease do
+    xo.unregister
+    ao.unregister
+    wo.unregister
+    ro.unregister
+    zo.unregister
+  end
+  HTimer.new(50)
+  .callback do
+    pool.request
+  end
+end
+
+def draw
+  pool.each do |d|
+    d.loc(d.x, d.y - rand(0.25..1), d.z)
+    d.no_stroke
+    d.fill(colors.get_color(d.x, d.y))
+    # if the z axis hits this range, change fill to light yellow
+    d.fill(color(0xffFFFFCC)) if d.z > -10 && d.z < 10
+    pool.release(d) if d.y < -40
+  end
+  H.draw_stage
+end
+
+def settings
+  size 640, 640, P3D
+end
--- external_library/java/hype/h_sprite.rb
+++ external_library/java/hype/h_sprite.rb
@@ -1,12 +1,14 @@
 #encoding: utf-8
 load_library :hype
 include_package 'hype'
-java_import 'hype.extended.behavior.HOrbiter3D'
-java_import 'hype.extended.behavior.HOscillator'
-java_import 'hype.extended.colorist.HColorPool'
+EXTENDED = %w[
+  colorist.HColorPool behavior.HOscillator behavior.HOrbiter3D
+].freeze
+hype_extended_format = 'hype.extended.%s'
+EXTENDED.each { |klass| java_import format(hype_extended_format, klass) }
 
 PALETTE = %w(#FFFFFF #F7F7F7 #ECECEC #CCCCCC #999999 #666666 #4D4D4D #333333 #FF3300 #FF6600).freeze
-attr_reader :pool
+attr_reader :pool,
 
 def settings
   size(640, 640, P3D)
@@ -16,7 +18,7 @@ def setup
   sketch_title 'Sprite'
   H.init(self)
   H.background(color('#242424'))
-  H.use3D(true)  
+  H.use3D(true)
   @pool = HDrawablePool.new(400)
   pool.auto_add_to_stage
       .add(HSprite.new(50,50).texture(HImage.new(data_path('tex1.png'))))
@@ -25,22 +27,20 @@ def setup
       .colorist(HColorPool.new(web_to_color_array(PALETTE)).fill_only)
       .on_create do |obj|
     i = pool.current_index
-    obj.anchorAt(H::CENTER).rotation(45)    
-    orb = HOrbiter3D.new(width / 2, height / 2, 0)
+    obj.anchorAt(H::CENTER).rotation(45)
+    HOrbiter3D.new(width / 2, height / 2, 0)
                     .target(obj)
                     .radius(225)
                     .ySpeed(1)
                     .zSpeed(1)
                     .yAngle((i + 1) *2)
-                    .zAngle((i + 1) *3)    
+                    .zAngle((i + 1) *3)
     HOscillator.new
                .target(obj)
                .property(H::SCALE)
                .range(0.1, 1.0)
                .speed(0.1)
                .freq(10)
-    
-    
     HOscillator.new
                .target(obj)
                .property(H::ROTATION)
@@ -48,7 +48,7 @@ def setup
                .speed(0.1)
                .freq(10)
                .current_step(i)
-  end.request_all  
+  end.request_all
 end
 
 def draw
