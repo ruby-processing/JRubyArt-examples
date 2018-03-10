ad3c5513512edec1c6af06ff24758a9f030a494f
monkstone <mamba2928@yahoo.co.uk>
2018-03-10 08:48:00 +0000

    Radical refactor

--- processing_app/library/library_proxy/library/dead_grid/dead_grid.rb
+++ processing_app/library/library_proxy/library/dead_grid/dead_grid.rb
@@ -1,56 +1,51 @@
 # By inheriting from LibraryProxy draw loop is by reflection sketch draw loop
 class DeadGrid < LibraryProxy
-  attr_reader :app, :block_size
-  attr_accessor :grid, :width, :height
+  include Processing::Proxy
+  attr_reader :app, :block_size, :grid, :width, :height
 
   def initialize(app)
     @app = app
     @block_size = app.block_size
     @width = app.width / block_size - 1
     @height = app.height / block_size - 1
-    @grid = Array.new
-    0.upto(width) do |i|
-      grid[i] = Array.new
-      0.upto(height) { |j| @grid[i][j] = 0 }
-    end
+    init_dead_grid(width, height)
   end
 
   def to_s
-    string = ""
-    string << "\nDeadGrid\n"
-    string << "-" * width << "\n"
-    0.upto(width) do |i|
-      0.upto(height) { |j| string << grid[j][i].to_s }
-      string << "\n"
+    "\nDeadGrid\n".tap do |string|
+      string << "-" * width << "\n"
+      grid.each do |row|
+        string << row.inspect << "\n"
+      end
     end
-    string
+  end
+
+  def init_dead_grid(width, height)
+    @grid = (0..width).map do |row|
+      (0..height).map { 0 }
+    end
   end
 
   def draw
-    app.stroke(255, 255, 255, 125)
-    app.stroke_weight(1)
-    0.upto(width) do |i|
-      0.upto(height) do |j|
-        x = i * block_size
-        y = j * block_size
-
-        if (@grid[i][j] != 0)
-          if (@grid[i][j] == 1)
-            app.fill(255, 0, 0)
-          elsif (@grid[i][j] == 2)
-            app.fill(120, 200, 50)
-          end
-          app.rect(x,y,block_size,block_size)
+    stroke(color(255, 255, 255, 125))
+    stroke_weight(1)
+    app.grid(width, height) do |row, column| # jruby_art convenience method grid
+      x = row * block_size
+      y = column * block_size
+      if (@grid[row][column] != 0)
+        if (@grid[row][column] == 1)
+          fill(color(255, 0, 0))
+        elsif (@grid[row][column] == 2)
+          fill(color(120, 200, 50))
         end
+        rect(x, y, block_size, block_size)
       end
     end
   end
 
   def randomize
-    0.upto(width) do |i|
-      0.upto(height) do |j|
-        @grid[i][j] = rand(0..1)
-      end
+    app.grid(width, height) do |row, column| # jruby_art convenience method grid
+      grid[row][column] = rand(0..1)
     end
   end
 
@@ -86,6 +81,7 @@ end
 
 # By inheriting from LibraryProxy draw loop is by reflection sketch draw loop
 class Block < LibraryProxy
+  include Processing::Proxy
   attr_reader :app, :block_size
   attr_accessor :x, :y, :width, :height, :direction
 
@@ -100,8 +96,8 @@ class Block < LibraryProxy
   end
 
   def draw
-    app.fill(55, 0, 255)
-    app.rect(@x * block_size, @y * block_size, @width, @height)
+    fill(color(55, 0, 255))
+    rect(@x * block_size, @y * block_size, @width, @height)
   end
 end
 
