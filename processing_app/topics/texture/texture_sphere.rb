b4825f3b0f85d456bdb9c8eb7ae7c2e06a34a957
monkstone <mamba2928@yahoo.co.uk>
2018-06-09 07:09:27 +0100

    Tidy up slightly

--- processing_app/topics/texture/texture_sphere.rb
+++ processing_app/topics/texture/texture_sphere.rb
@@ -4,11 +4,9 @@
 # by Mike 'Flux' Chang (and cleaned up by Aaron Koblin). Original based on
 # code by Toxi. Translated to JRubyArt and transformed by Martin Prout
 #
-# A 3D textured sphere with ArcBall.
-
-
+# Textured sphere with ArcBall, zoom using mousewheel & rotate with mouse drag
 attr_reader :res_w, :res_h, :img, :points_width, :points_height, :points_total
-attr_reader :coorX, :coorY, :coorZ, :multXZ
+attr_reader :coor_x, :coor_y, :coor_z, :multXZ
 
 def setup
   sketch_title 'Textured Sphere'
@@ -24,7 +22,7 @@ end
 
 # Use arrow keys to change detail settings
 def key_pressed
-  case (keyCode)
+  case keyCode
   when ENTER
     save_frame
   when UP
@@ -46,63 +44,50 @@ def draw
 end
 
 def initialize_sphere(res_x, res_y)
-
   # The number of points around the width and height
   @points_width = res_x + 1
   @points_total = res_y # How many actual pts around the sphere (not just from top to bottom)
   @points_height = (points_total / 2.0).to_i + 1 # How many pts from top to bottom (abs(....) b/c of the possibility of an odd points_total)
-
-  @coorX = Array.new(points_width) # All the x-coor in a horizontal circle radius 1
-  @coorY = Array.new(points_height) # All the y-coor in a vertical circle radius 1
-  @coorZ = Array.new(points_width) # All the z-coor in a horizontal circle radius 1
-  @multXZ = Array.new(points_height) # The radius of each horizontal circle (that you will multiply with coorX and coorZ)
-
+  @coor_x = Array.new(points_width) # All the x-coor in a horizontal circle radius 1
+  @coor_y = Array.new(points_height) # All the y-coor in a vertical circle radius 1
+  @coor_z = Array.new(points_width) # All the z-coor in a horizontal circle radius 1
+  @multXZ = Array.new(points_height) # The radius of each horizontal circle (that you will multiply with coor_x and coor_z)
   points_width.times do |i| # For all the points around the width
-    thetaW = i * 2 * PI / (points_width - 1)
-    coorX[i] = sin(thetaW)
-    coorZ[i] = cos(thetaW)
+    theta_w = i * TWO_PI / (points_width - 1)
+    coor_x[i] = sin(theta_w)
+    coor_z[i] = cos(theta_w)
   end
-
   points_height.times do |i| # For all points from top to bottom
-    if points_total.odd? && i == points_height - 1  # If the points_total is odd and it is at the last pt
-      thetaH = (i - 1) * 2 * PI / (points_total)
-      coorY[i] = cos(PI + thetaH)
-      multXZ[i] = 0
-    else
-      #The points_total and 2 below allows there to be a flat bottom if the points_height is odd
-      thetaH = i * 2 * PI / (points_total)
-      #PI+ below makes the top always the point instead of the bottom.
-      coorY[i] = cos(PI + thetaH)
-      multXZ[i] = sin(thetaH)
-    end
+    last_point_odd = points_total.odd? && i == points_height - 1
+    numerator = last_point_odd ? (i - 1) * TWO_PI : i * TWO_PI
+    theta_h = numerator / points_total
+    multXZ[i] = last_point_odd ? 0 : sin(theta_h) # sin(0) == 0
+    coor_y[i] = cos(PI + theta_h)
   end
 end
 
 def texture_sphere(rx, ry, rz, image)
   # These are so we can map certain parts of the image on to the shape
-  changeU = image.width / (points_width - 1).to_f
-  changeV = image.height / (points_height - 1).to_f
+  delta_u = image.width / (points_width - 1).to_f
+  delta_v = image.height / (points_height - 1).to_f
   u = 0 # Width variable for the texture
   v = 0 # Height variable for the texture
-
   begin_shape(TRIANGLE_STRIP)
   texture(image)
   (0...points_height - 1).each do |i|  # For all the rings but top and bottom
     # Goes into the array here instead of loop to save time
-    coory = coorY[i]
-    cooryPlus = coorY[i + 1]
-
+    coory = coor_y[i]
+    coory_succ = coor_y[i.succ] # use succ (successor) rubyists should be happy
     multxz = multXZ[i]
-    multxzPlus = multXZ[i + 1]
-
+    multxz_succ = multXZ[i.succ]
     (0...points_width).each do |j| # For all the pts in the ring
-      normal(-coorX[j] * multxz, -coory, -coorZ[j] * multxz)
-      vertex(coorX[j] * multxz * rx, coory * ry, coorZ[j] * multxz * rz, u, v)
-      normal(-coorX[j] * multxzPlus, -cooryPlus, -coorZ[j] * multxzPlus)
-      vertex(coorX[j] * multxzPlus * rx, cooryPlus * ry, coorZ[j] * multxzPlus * rz, u, v + changeV)
-      u += changeU
+      normal(-coor_x[j] * multxz, -coory, -coor_z[j] * multxz)
+      vertex(coor_x[j] * multxz * rx, coory * ry, coor_z[j] * multxz * rz, u, v)
+      normal(-coor_x[j] * multxz_succ, -coory_succ, -coor_z[j] * multxz_succ)
+      vertex(coor_x[j] * multxz_succ * rx, coory_succ * ry, coor_z[j] * multxz_succ * rz, u, v + delta_v)
+      u += delta_u
     end
-    v += changeV
+    v += delta_v
     u = 0
   end
   end_shape
