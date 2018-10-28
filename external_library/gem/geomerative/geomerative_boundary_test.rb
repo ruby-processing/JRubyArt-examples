384960da9fb4a4755ccb0d9d2e1eff8f7c1a52d0
Martin Prout <mamba2928@yahoo.co.uk>
2018-10-28 15:59:35 +0000

    geomerative boundary

---
+++ external_library/gem/geomerative/geomerative_boundary_test.rb
@@ -0,0 +1,34 @@
+require 'geomerative'
+load_library :boundary
+
+attr_reader :bounds, :my_rect
+
+def settings
+  size(600, 480, P2D)
+end
+
+def setup
+  sketch_title 'Geomerative Boundary Test'
+  RG.init(self)
+  RG.set_polygonizer(RG.ADAPTATIVE)
+  @bounds = Boundary.create_bounding_rectangle(100, 100, 100, 50)
+end
+
+def draw
+  fill 255
+  stroke 0
+  @my_rect = RShape.create_rectangle(mouseX, mouseY, 10, 10)
+  bounds.draw
+  draw_my_rect
+end
+
+def draw_my_rect
+  if bounds.inside(my_rect)
+    no_stroke
+    fill 255, 0, 0
+  else
+    stroke 0
+    fill 255
+  end
+  my_rect.draw
+end
