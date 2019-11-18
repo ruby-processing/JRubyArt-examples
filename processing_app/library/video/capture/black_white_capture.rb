a914ff1ac57abdd7e08efa7dbeda7508fe193491
Martin Prout <mamba2928@yahoo.co.uk>
2019-11-18 19:20:46 +0000

    Update to work with latest processinf video beta release

--- processing_app/library/video/capture/black_white_capture.rb
+++ processing_app/library/video/capture/black_white_capture.rb
@@ -6,11 +6,11 @@ attr_reader :cam, :my_shader
 def setup
   sketch_title 'Black & White Capture'
   @my_shader = load_shader(data_path('bwfrag.glsl'))
-  start_capture(width, height)
+  start_capture
 end
 
-def start_capture(w, h)
-  @cam = Java::ProcessingVideo::Capture.new(self, w, h)
+def start_capture
+  @cam = Java::ProcessingVideo::Capture.new(self, "UVC Camera (046d:0825)")
   cam.start
 end
 
@@ -21,10 +21,10 @@ def draw
 end
 
 # using snake case to match java reflect method
-def captureEvent(c)
-  c.read
+def captureEvent(cam)
+  cam.read
 end
 
 def settings
-  size(960, 544, P2D)
+  size(480, 360, P2D)
 end
--- processing_app/library/video/capture/test_capture.rb
+++ processing_app/library/video/capture/test_capture.rb
@@ -7,13 +7,8 @@ attr_reader :cam
 def setup
   sketch_title 'Test Capture'
   cameras = Capture.list
-  fail 'There are no cameras available for capture.' if cameras.length.zero?
-  p 'Matching cameras available:'
-  size_pattern = Regexp.new(format('%dx%d', width, height))
-  select = cameras.grep size_pattern # filter available cameras
-  select.uniq.map { |cam| p cam.strip }
-  fail 'There are no matching cameras.' if select.length.zero?
-  start_capture(select[0])
+  fail 'There are no matching cameras.' if cameras.length.zero?
+  start_capture(cameras[0])
 end
 
 def start_capture(cam_string)
@@ -36,5 +31,5 @@ def captureEvent(c)
 end
 
 def settings
-  size 1280, 720, P2D
+  size 960, 720, P2D
 end
