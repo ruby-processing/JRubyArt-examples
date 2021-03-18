dffed4ce49ac76e25745bf075febe0ec28182c5a
monkstone <mamba2928@yahoo.co.uk>
2021-03-18 16:00:05 +0000

    more mods

--- external_library/gem/toxiclibs/noise/simplex_noise_texture.rb
+++ external_library/gem/toxiclibs/noise/simplex_noise_texture.rb
@@ -3,25 +3,21 @@ require 'toxiclibs'
 # by Martin Prout.
 # Using noise to create simple texture.
 # control parameter with mouse
+DELTA = 0.02
 
 def setup
   sketch_title 'Noisy Texture'
-  @increment = 0.02
 end
 
 def draw
   background 0
   load_pixels
-  xoff = 0.0
-  x_val = map1d(mouse_x, 0..width, 1..100)
-  (0...width).each do |x|
-    xoff += @increment
-    yoff = 0.0
-    (0...height).each do |y|
-      yoff += @increment
-      bright = Toxi::SimplexNoise.noise(x / x_val, y / x_val) * 255
-      pixels[x + y * width] = color(bright)
-    end
+  x_val = map1d(mouse_x, 0..width, 1..10)
+  grid(width, height) do |x, y|
+    bright = (
+      Toxi::SimplexNoise.noise(x * DELTA / x_val, y * DELTA / x_val) + 1
+      ) * 128
+    pixels[x + y * width] = color(bright)
   end
   update_pixels
 end
