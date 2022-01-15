691f481e4e9d21fafdca83314753bc9d4cc01cac
monkstone <mamba2928@yahoo.co.uk>
2022-01-15 08:58:55 +0000

    Simplify code using ArcBall and GfxRender classes

--- processing_app/basics/form/rgb_cube.rb
+++ processing_app/basics/form/rgb_cube.rb
@@ -1,3 +1,4 @@
+java_import 'monkstone.vecmath.GfxRender'
 # RGB Cube.
 #
 # The three primary colors of the additive color model are red, green, and blue.
@@ -8,63 +9,56 @@ attr_reader :box_points
 def setup
   sketch_title 'RGB Cube'
   no_stroke
-  color_mode RGB, 2
-  @xmag = 0
-  @ymag = 0
-  @new_xmag = 0
-  @new_ymag = 0
-  # Math.since each point is used three times
+  ArcBall.init(self, width / 2, height / 2)
+
   @box_points = {
-    top_front_left: [-1,  1,  1],
-    top_front_right: [1,  1,  1],
-    top_back_right: [1,  1, -1],
-    top_back_left: [-1,  1, -1],
-    bottom_front_left: [-1, -1,  1],
-    bottom_front_right: [1, -1,  1],
-    bottom_back_right: [1, -1, -1],
-    bottom_back_left: [-1, -1, -1]
+    top_front_left: Vec3D.new(-90,  90,  90),
+    top_front_right: Vec3D.new(90,  90,  90),
+    top_back_right: Vec3D.new(90,  90, -90),
+    top_back_left: Vec3D.new(-90,  90, -90),
+    bottom_front_left: Vec3D.new(-90, -90,  90),
+    bottom_front_right: Vec3D.new(90, -90,  90),
+    bottom_back_right: Vec3D.new(90, -90, -90),
+    bottom_back_left: Vec3D.new(-90, -90, -90)
   }
   # a box from defined points
   @box = {
-    top: [box_points[:top_front_left], box_points[:top_front_right], box_points[:top_back_right], box_points[:top_back_left]],
-    front: [box_points[:top_front_left], box_points[:top_front_right], box_points[:bottom_front_right], box_points[:bottom_front_left]],
-    left: [box_points[:top_front_left], box_points[:bottom_front_left], box_points[:bottom_back_left], box_points[:top_back_left]],
-    back: [box_points[:top_back_left], box_points[:top_back_right], box_points[:bottom_back_right], box_points[:bottom_back_left]],
-    right: [box_points[:top_back_right], box_points[:bottom_back_right], box_points[:bottom_front_right], box_points[:top_front_right]],
-    bottom: [box_points[:bottom_front_left], box_points[:bottom_front_right], box_points[:bottom_back_right], box_points[:bottom_back_left]]
+    top: [box_points[:top_front_left], box_points[:top_front_right], box_points[:top_back_right],
+    box_points[:top_back_left]],
+    front: [box_points[:top_front_left], box_points[:top_front_right], box_points[:bottom_front_right],
+    box_points[:bottom_front_left]],
+    left: [box_points[:top_front_left], box_points[:bottom_front_left], box_points[:bottom_back_left],
+    box_points[:top_back_left]],
+    back: [box_points[:top_back_left], box_points[:top_back_right], box_points[:bottom_back_right],
+    box_points[:bottom_back_left]],
+    right: [box_points[:top_back_right], box_points[:bottom_back_right], box_points[:bottom_front_right],
+    box_points[:top_front_right]],
+    bottom: [box_points[:bottom_front_left], box_points[:bottom_front_right], box_points[:bottom_back_right],
+    box_points[:bottom_back_left]]
   }
 end
 
 def draw
   background 1
-  push_matrix
-  translate width / 2, height / 2, -30
-  @new_xmag = mouse_x.to_f / width * TAU
-  @new_ymag = mouse_y.to_f / height * TAU
-  diff = @xmag - @new_xmag
-  @xmag -= diff / 4 if diff.abs > 0.01
-  diff = @ymag - @new_ymag
-  @ymag -= diff / 4 if diff.abs > 0.01
-  rotate_x(-@ymag)
-  rotate_y(-@xmag)
-  scale 90
   begin_shape QUADS
   %i[top front left back right bottom].each do |s|
     @box[s].each do |p|
       fill_from_points p
-      vertex_from_points p
+      p.to_vertex(renderer)
     end
   end
   end_shape
-  pop_matrix
 end
 
 def fill_from_points(points)
-  fill points[0] + 1, points[1] + 1, points[2] + 1 # "+1" translates -1,1 to 0,2
+  red = map1d(points.x, -90..90, 0..255)
+  blue = map1d(points.y, -90..90, 0..255)
+  green = map1d(points.z, -90..90, 0..255)
+  fill(red, blue, green)
 end
 
-def vertex_from_points(points)
-  vertex(*points)
+def renderer
+  @renderer ||= GfxRender.new(g)
 end
 
 def settings
